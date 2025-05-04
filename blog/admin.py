1abc9296064c49432ca74c421c582040f5180757
DermisZabala <dermiszabala@gmail.com>
2025-05-03 21:58:35 -0400

    Mi aplicación Django, primer commit

---
+++ blog/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Publicar # importando el modelo Publicar
+
+# Registrar tu modelo aqui.
+
+admin.site.register(Publicar)
---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+class Publicar(models.Model):
+    autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=200)
+    texto = models.TextField()
+    fecha_creacion = models.DateTimeField(default=timezone.now)
+    fecha_publicacion = models.DateField(blank=True, null=True)
+
+    def publicacion(self):
+        self.fecha_publicacion = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.titulo
